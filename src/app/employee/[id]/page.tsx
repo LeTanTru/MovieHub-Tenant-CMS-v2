f9f705e2f02f53791277a3e04679f0d8a72ab617
LeTanTru <letantru@gmail.com>
2026-01-21 20:24:38 +0700

    build: update

--- src/app/employee/[id]/page.tsx
+++ src/app/employee/[id]/page.tsx
@@ -1,36 +1,6 @@
 import { EmployeeForm } from '@/app/employee/_components';
-import { getQueryClient } from '@/components/providers/query-provider/get-query-provider';
-import { apiConfig, queryKeys } from '@/constants';
-import { ApiResponse, EmployeeResType } from '@/types';
-import { http } from '@/utils';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { queryKeys } from '@/constants';
 
-async function getEmployee(id: string) {
-  const res = await http.get<ApiResponse<EmployeeResType>>(
-    apiConfig.employee.getById,
-    {
-      pathParams: {
-        id
-      }
-    }
-  );
-  return res.data;
-}
-
-export default async function EmployeeSavePage(
-  props: PageProps<'/employee/[id]'>
-) {
-  // const { id } = await props.params;
-  // const queryClient = getQueryClient();
-
-  // await queryClient.prefetchQuery({
-  //   queryKey: [queryKeys.EMPLOYEE, { id }],
-  //   queryFn: () => getEmployee(id)
-  // });
-
-  return (
-    // <HydrationBoundary state={dehydrate(queryClient)}>
-    <EmployeeForm queryKey={queryKeys.EMPLOYEE} />
-    // </HydrationBoundary>
-  );
+export default function EmployeeSavePage() {
+  return <EmployeeForm queryKey={queryKeys.EMPLOYEE} />;
 }
